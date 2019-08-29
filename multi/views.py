2f215a442cbc95663c9e0ca5040b53e52f2ea6a4
MaiconMares <maiconmaresunb@gmail.com>
2019-08-29 03:00:49 +0000

    Implementação da validação do formulário

--- multi/views.py
+++ multi/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MultiForm
+from django.http import HttpResponse
 
 # Create your views here.
-def multi_number(request):
-    return render(request, 'multi/Multi.html', {})
+def  multi_multiplication(request):
+    form = MultiForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.res = form.multi()
+            post.save()    
+            return render(request, 'multi/Multi.html', {'form': form})
+        else:
+            print(form.errors)
+            return render(request, 'multi/Multi.html', {'form': form})
+    else:
+        return render(request, 'multi/Multi.html', {'form': form})
