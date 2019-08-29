8314872ab45af90cd43c77cbe9d5b3869cb37035
MaiconMares <maiconmaresunb@gmail.com>
2019-08-29 02:57:09 +0000

    Exclusão de alguns métodos que foram passados para o forms ou excluídos sem aproveitamento

--- multi/models.py
+++ multi/models.py
@@ -4,12 +4,4 @@ from django.db import models
 class Multi(models.Model):
     number_x = models.IntegerField(null = False)
     number_y = models.IntegerField(null = False)
-    res = models.IntegerField(null = False)
-
-    def __init__(self):
-        self.multi()
-
-    def multi(self, x = 0, y = 0):
-        self.number_x = x
-        self.number_y = y
-        self.res = self.number_x * self.number_y    
+    res = models.IntegerField(null=False)
